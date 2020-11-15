71c952572192ffde448b351c778c8ed4641c4e61
Van Anthony Silleza <van.silleza@gmail.com>
2020-11-16 02:21:49 +0800

    first commit

---
+++ CASHIER/urls.py
@@ -0,0 +1,45 @@
+"""CASHIER URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from CASHIERING import views
+
+urlpatterns = [
+    path('login', views.login, name='login'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('add_collection_manual', views.add_collection_manual, name='add_collection_manual'),
+    path('add_collection_student', views.add_collection_student, name='add_collection_student'),
+    path('view_receipts', views.view_receipts, name='view_receipts'),
+    path('show_receipt', views.show_receipt, name='show_receipt'),
+    path('cleared_students', views.cleared_students, name='cleared_students'),
+    path('sis_payments', views.sis_payments, name='sis_payments'),
+    path('or_setup', views.or_setup, name='or_setup'),
+    path('accountable_forms_report', views.accountable_forms_report, name='accountable_forms_report'),
+    path('certification_report', views.certification_report, name='certification_report'),
+    path('monthly_collection_report', views.monthly_collection_report, name='monthly_collection_report'),
+    path('summary_collection_report', views.summary_collection_report, name='summary_collection_report'),
+    path('cash_receipt_record_report', views.cash_receipt_record_report, name='cash_receipt_record_report'),
+    path('deposits_pending', views.deposits_pending, name='deposits_pending'),
+    path('admin_dashboard', views.admin_dashboard, name='admin_dashboard'),
+    path('add_user', views.add_user, name='add_user'),
+    path('edit_user', views.edit_user, name='edit_user'),
+    path('react_deact', views.react_deact, name='react_deact'),
+    path('setup_uacs', views.setup_uacs, name='setup_uacs'),
+    path('email', views.email, name="email"),
+    path('account', views.account, name="account"),
+]
---
+++ CASHIERING/views.py
@@ -0,0 +1,80 @@
+from django.conf import settings
+from django.shortcuts import render, redirect, resolve_url
+from django.db import connection
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.cache import cache_control
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+# Create your views here.
+
+def login(request):
+    return render(request, 'views/layouts/pages/login.html')
+
+def dashboard(request):
+    return render(request, 'views/layouts/pages/dashboard.html')
+
+def add_collection_manual(request):
+    return render(request, 'views/layouts/pages/add_collection_manual.html')
+
+def add_collection_student(request):
+    return render(request, 'views/layouts/pages/add_collection_student.html')
+
+def view_receipts(request):
+    return render(request, 'views/layouts/pages/view_receipts.html')
+
+def show_receipt(request):
+    return render(request, 'views/layouts/pages/show_receipt.html')
+
+def cleared_students(request):
+    return render(request, 'views/layouts/pages/cleared_students.html')
+
+def sis_payments(request):
+    return render(request, 'views/layouts/pages/sis_payments.html')
+
+def or_setup(request):
+    return render(request, 'views/layouts/pages/or_setup.html')
+
+def accountable_forms_report(request):
+    return render(request, 'views/layouts/pages/accountable_forms_report.html')
+
+def certification_report(request):
+    return render(request, 'views/layouts/pages/certification_report.html')
+
+def monthly_collection_report(request):
+    return render(request, 'views/layouts/pages/monthly_collection_report.html')
+
+def summary_collection_report(request):
+    return render(request, 'views/layouts/pages/summary_collection_report.html')
+
+def cash_receipt_record_report(request):
+    return render(request, 'views/layouts/pages/cash_receipt_record_report.html')
+
+def deposits_pending(request):
+    return render(request, 'views/layouts/pages/deposits_pending.html')
+
+def deposits_deposited(request):
+    return render(request, 'views/layouts/pages/deposits_deposited.html')
+
+def admin_dashboard(request):
+    return render(request, 'views/layouts/pages/admin_dashboard.html')
+
+def add_user(request):
+    return render(request, 'views/layouts/pages/add_user.html')
+
+def edit_user(request):
+    return render(request, 'views/layouts/pages/edit_user.html')
+
+def react_deact(request):
+    return render(request, 'views/layouts/pages/react_deact.html')
+
+def setup_uacs(request):
+    return render(request, 'views/layouts/pages/setup_uacs.html')
+
+def email(request):
+    return render(request, 'views/layouts/pages/email.html')
+
+def account(request):
+    return render(request, 'views/layouts/pages/account.html')
