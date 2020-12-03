b567551c4a4436a722f0a4e6d377754daa8db7ad
Van Anthony Silleza <van.silleza@gmail.com>
2020-12-03 15:54:31 +0800

    add changes

--- CASHIERING/views.py
+++ CASHIERING/views.py
@@ -16,87 +16,73 @@ def login(request):
 def dashboard(request):
     return render(request, 'views/layouts/pages/dashboard.html')
 
-@login_required
 def add_collection_manual(request):
     return render(request, 'views/layouts/pages/add_collection_manual.html')
 
-@login_required
 def add_collection_student(request):
     return render(request, 'views/layouts/pages/add_collection_student.html')
 
-@login_required
 def view_receipts(request):
     return render(request, 'views/layouts/pages/view_receipts.html')
 
-@login_required
 def show_receipt(request):
     return render(request, 'views/layouts/pages/show_receipt.html')
 
-@login_required
 def cleared_students(request):
     return render(request, 'views/layouts/pages/cleared_students.html')
 
-@login_required
 def sis_payments(request):
     return render(request, 'views/layouts/pages/sis_payments.html')
 
-@login_required
 def or_setup(request):
     return render(request, 'views/layouts/pages/or_setup.html')
 
-@login_required
 def accountable_forms_report(request):
     return render(request, 'views/layouts/pages/accountable_forms_report.html')
 
-@login_required
 def certification_report(request):
     return render(request, 'views/layouts/pages/certification_report.html')
 
-@login_required
 def monthly_collection_report(request):
     return render(request, 'views/layouts/pages/monthly_collection_report.html')
 
-@login_required
 def summary_collection_report(request):
     return render(request, 'views/layouts/pages/summary_collection_report.html')
 
-@login_required
 def cash_receipt_record_report(request):
     return render(request, 'views/layouts/pages/cash_receipt_record_report.html')
 
-@login_required
 def deposits_pending(request):
     return render(request, 'views/layouts/pages/deposits_pending.html')
 
-@login_required
+
 def deposits_deposited(request):
     return render(request, 'views/layouts/pages/deposits_deposited.html')
 
-@login_required
+
 def admin_dashboard(request):
     return render(request, 'views/layouts/pages/admin_dashboard.html')
 
-@login_required
+
 def add_user(request):
     return render(request, 'views/layouts/pages/add_user.html')
 
-@login_required
 def edit_user(request):
     return render(request, 'views/layouts/pages/edit_user.html')
 
-@login_required
+
 def react_deact(request):
     return render(request, 'views/layouts/pages/react_deact.html')
 
-@login_required
+
 def setup_uacs(request):
     return render(request, 'views/layouts/pages/setup_uacs.html')
 
-@login_required
+
 def email(request):
     return render(request, 'views/layouts/pages/email.html')
 
-@login_required
+
 def account(request):
     return render(request, 'views/layouts/pages/account.html')
 
@@ -143,7 +129,7 @@ def get_session(request):
     role = request.session.get('role')
     return JsonResponse(role, safe=False)
 
-@login_required
+
 def error_403(request):
     return render(request, 'views/layouts/pages/error_403.html')
 
