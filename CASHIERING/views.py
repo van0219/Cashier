3b5303c3942b8471493102c437b5e281b2ed311f
Van Anthony Silleza <van.silleza@gmail.com>
2021-05-29 09:48:43 +0800

    add changes

--- CASHIERING/views.py
+++ CASHIERING/views.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 from django.conf import settings
 from django.shortcuts import render, redirect, resolve_url
 from django.db import IntegrityError, connection
@@ -5,6 +6,8 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.cache import cache_control
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+
+from fpdf import *
 
 
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
@@ -665,4 +668,88 @@ def load_top_contrib(request):
                     ,(request.POST['month']))
     data = cursor.fetchall()
     return JsonResponse(data, safe=False)
+
+def certification_pdf():
+    class PDF(FPDF):
+        # Load data
+        def load_data(self, name):
+            # Read file lines
+            data = []
+            with open(name) as file:
+                for line in file:
+                    data += [line[:-1].split(';')]
+            return data
+
+        # Simple table
+        def basic_table(self, header, data):
+            # Header
+            for col in header:
+                self.cell(40, 7, col, 1)
+            self.ln()
+            # Data
+            for row in data:
+                for col in row:
+                    self.cell(40, 6, col, 1)
+                self.ln()
+
+        # Better table
+        def improved_table(self, header, data):
+            # Column widths
+            w = [40, 35, 40, 45]
+            # Header
+            for i in range(0, len(header)):
+                self.cell(w[i], 7, header[i], 1, 0, 'C')
+            self.ln()
+            # Data
+            for row in data:
+                self.cell(w[0], 6, row[0], 'LR')
+                self.cell(w[1], 6, row[1], 'LR')
+                self.cell(w[2], 6, row[2], 'LR', 0, 'R')
+                self.cell(w[3], 6, row[3], 'LR', 0, 'R')
+                self.ln()
+            # Closure line
+            self.cell(sum(w), 0, '', 'T')
+
+        # Colored table
+        def fancy_table(self, header, data):
+            # Colors, line width and bold font
+            self.set_fill_color(255, 0, 0)
+            self.set_text_color(255)
+            self.set_draw_color(128, 0, 0)
+            self.set_line_width(0.3)
+            self.set_font('', 'B')
+            # Header
+            w = [40, 35, 40, 45]
+            for i in range(0, len(header)):
+                self.cell(w[i], 7, header[i], 1, 0, 'C', 1)
+            self.ln()
+            # Color and font restoration
+            self.set_fill_color(224, 235, 255)
+            self.set_text_color(0)
+            self.set_font('')
+            # Data
+            fill=0
+            for row in data:
+                self.cell(w[0], 6, row[0], 'LR', 0, 'L', fill)
+                self.cell(w[1], 6, row[1], 'LR', 0, 'L', fill)
+                self.cell(w[2], 6, row[2], 'LR', 0, 'R', fill)
+                self.cell(w[3], 6, row[3], 'LR', 0, 'R', fill)
+                self.ln()
+                fill = not fill
+            self.cell(sum(w), 0, '', 'T')
+
+    pdf = PDF()
+    # Column titles
+    header = ['Country', 'Capital', 'Area (sq km)', 'Pop. (thousands)']
+    # Data loading
+    data = pdf.load_data('countries.txt')
+    pdf.set_font('Arial', '', 14)
+    # pdf.set_margins(left=20, top=20)
+    pdf.add_page()
+    pdf.basic_table(header, data)
+    pdf.add_page()
+    pdf.improved_table(header, data)
+    pdf.add_page()
+    pdf.fancy_table(header, data)
+    pdf.output('tuto5.pdf', 'F')
 
